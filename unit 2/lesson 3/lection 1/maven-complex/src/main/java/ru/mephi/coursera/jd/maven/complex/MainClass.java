290ded53253ff5339b2cf891eab73be75dda24b4
androsovgr <androsovgr@gmail.com>
2017-02-18 20:03:10 +0300

    last maven example

---
+++ unit 2/lesson 3/lection 1/maven-complex/src/main/java/ru/mephi/coursera/jd/maven/complex/MainClass.java
@@ -0,0 +1,9 @@
+package ru.mephi.coursera.jd.maven.complex;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println(StringUtils.abbreviate("string very long", 7));
+	}
+}
